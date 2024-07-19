9554d5653c09687c53a0f60886810ccf4caf13be
Antonio Mendes <raymendesjr2013@gmail.com>
2024-07-19 10:05:13 -0300

    add: resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,305 @@
+"use client";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { motion } from "framer-motion";
+import {
+  FaCss3,
+  FaCss3Alt,
+  FaGit,
+  FaGithub,
+  FaHtml5,
+  FaJs,
+  FaNodeJs,
+  FaReact,
+} from "react-icons/fa";
+import { SiNextdotjs, SiTailwindcss } from "react-icons/si";
+
+const about = {
+  title: "About Me",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+  infos: [
+    {
+      title: "Name",
+      content: "Antonio Mendes",
+    },
+    {
+      title: "Location",
+      content: "Brazil",
+    },
+    {
+      title: "Experience",
+      content: "5+ Years",
+    },
+    {
+      title: "Languages",
+      content: "Portuguese, English",
+    },
+    {
+      title: "Freelance",
+      content: "Available",
+    },
+    {
+      title: "Email",
+      content: "mendes.antonioraian@gmail.com",
+    },
+  ],
+};
+
+const experiences = {
+  icon: "/assets/resume/badge.svg",
+  title: "My Experience",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+  itens: [
+    {
+      company: "Company Name",
+      position: "Position",
+      date: "2020 - 2021",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+    },
+    {
+      company: "Company Name",
+      position: "Position",
+      date: "2020 - 2021",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+    },
+    {
+      company: "Company Name",
+      position: "Position",
+      date: "2020 - 2021",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+    },
+    {
+      company: "Company Name",
+      position: "Position",
+      date: "2020 - 2021",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+    },
+  ],
+};
+
+const educations = {
+  icon: "/assets/resume/cap.svg",
+  title: "My Education",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+  itens: [
+    {
+      instituition: "Instituition Name",
+      degree: "Position",
+      date: "2020 - 2021",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+    },
+    {
+      instituition: "Instituition Name",
+      degree: "Position",
+      date: "2020 - 2021",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+    },
+    {
+      instituition: "Instituition Name",
+      degree: "Position",
+      date: "2020 - 2021",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+    },
+    {
+      instituition: "Instituition Name",
+      degree: "Position",
+      date: "2020 - 2021",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+    },
+  ],
+};
+
+const skills = {
+  title: "My Skills",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+  itens: [
+    {
+      name: "HTML 5",
+      icon: <FaHtml5 />,
+    },
+    {
+      name: "CSS 3",
+      icon: <FaCss3 />,
+    },
+    {
+      name: "Javascript",
+      icon: <FaJs />,
+    },
+    {
+      name: "React",
+      icon: <FaReact />,
+    },
+    {
+      name: "Next JS",
+      icon: <SiNextdotjs />,
+    },
+    {
+      name: "Tailwind",
+      icon: <SiTailwindcss />,
+    },
+    {
+      name: "Git",
+      icon: <FaGit />,
+    },
+    {
+      name: "Node JS",
+      icon: <FaNodeJs />,
+    },
+  ],
+};
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex justify-center items-center py-12 lg:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col lg:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto lg:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About Me</TabsTrigger>
+          </TabsList>
+
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center lg:text-left">
+                <h3 className="text-4xl font-bold">{experiences.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto lg:mx-0">
+                  {experiences.description}
+                </p>
+                <ScrollArea className="w-full h-[400px]">
+                  <ul className="grid grid-cols-1 md:grid-cols-2 gap-[30px]">
+                    {experiences.itens.map((item, index) => (
+                      <li
+                        key={index}
+                        className="flex flex-col justify-center items-center 
+                      bg-[#232329] h-[184px] py-6 px-10 rounded-xl lg:items-start gap-1"
+                      >
+                        <span className="text-accent">{item.date}</span>
+                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                          {item.position}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.company}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center lg:text-left">
+                <h3 className="text-4xl font-bold">{educations.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto lg:mx-0">
+                  {educations.description}
+                </p>
+                <ScrollArea className="w-full h-[400px]">
+                  <ul className="grid grid-cols-1 md:grid-cols-2 gap-[30px]">
+                    {educations.itens.map((item, index) => (
+                      <li
+                        key={index}
+                        className="flex flex-col justify-center items-center 
+                      bg-[#232329] h-[184px] py-6 px-10 rounded-xl lg:items-start gap-1"
+                      >
+                        <span className="text-accent">{item.date}</span>
+                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                          {item.degree}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.instituition}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center lg:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto lg:mx-0">
+                    {skills.description}
+                  </p>
+                </div>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:gap-[30px] gap-4">
+                  {skills.itens.map((item, index) => (
+                    <li key={index} className="">
+                      <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                          <TooltipTrigger className="w-full h-[150px] rounded-xl flex justify-center items-center bg-[#232329] group ">
+                            <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                              {item.icon}
+                            </div>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p className="capitalize">{item.name}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </TabsContent>
+            <TabsContent
+              value="about"
+              className="w-full text-center lg:text-left"
+            >
+              <div className="flex flex-col gap-[30px] text-center lg:text-left">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto lg:mx-0">
+                  {about.description}
+                </p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto lg:mx-0">
+                  {about.infos.map((info, index) => (
+                    <li
+                      key={index}
+                      className="flex justify-center items-center lg:justify-start gap-4"
+                    >
+                      <span className="text-accent">{info.title}</span>
+                      <span className="text-xl">{info.content}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
