e920a2dfceeb1dca178942d27f18ff09716baef7
Antonio Mendes <raymendesjr2013@gmail.com>
2024-12-14 18:45:33 -0300

    fix: Contact Route

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,13 +1,8 @@
 import { sendMail } from "@/lib/send-mail";
-import { NextApiRequest, NextApiResponse } from "next";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Método não permitido." });
-  }
-
+export async function POST(req: Request) {
   try {
-    const { name, email, phone, message, recaptchaToken } = req.body;
+    const { name, email, phone, message, recaptchaToken } = await req.json();
 
     const mailText = `Nome: ${name}\nE-mail: ${email}\nTelefone: ${phone}\nMensagem: ${message}`;
 
@@ -15,16 +10,16 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
     const secretKey = process.env.NEXT_SECRET_RECAPTCHA_SITE_KEY;
     const recaptchaResponse = await fetch(
       `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptchaToken}`,
-      { method: "POST" }
+      { method: "POST", body: "" }
     );
     const recaptchaData = await recaptchaResponse.json();
 
     console.log({ recaptchaData });
 
     if (!recaptchaData.success || recaptchaData.score < 0.5) {
-      return res
-        .status(400)
-        .json({ message: "Falha na verificação do reCAPTCHA." });
+      return Response.json({
+        message: "Falha na verificação do reCAPTCHA.",
+      });
     }
 
     const resp = await sendMail({
@@ -34,10 +29,10 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
     });
 
     if (resp?.messageId)
-      res.status(200).json({ message: "Email enviado com sucesso!" });
-    else res.status(500).json({ message: "Erro ao enviar o email." });
+      Response.json({ message: "Email enviado com sucesso!" });
+    else Response.error();
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Erro ao processar a requisição." });
+    console.log({ error });
+    Response.json({ message: "Erro ao processar a requisição." });
   }
 }
